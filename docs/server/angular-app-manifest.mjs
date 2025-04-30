3a38ffe168a32e916627a138437ee77ab248aae2
Sameer Akhter <akhtersameer736@gmail.com>
2025-05-01 01:24:09 +0530

    Update

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -17,9 +17,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 10280, hash: 'fb8dc69a3f89fe9bb85d022aea8811c823241313404e86ea34d0c76abbfe941c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1199, hash: 'ccc7925fbe1de69a3d2497a6f57490149c2c74298f66a855dc1de13e89eb8278', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 44524, hash: '530188fa084555ae0e0b085b6d1669f22c8d4d0c6c2f9b465d9959eb96a87125', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-DUQBEOLS.css': {size: 409148, hash: 'K5cgbR4f3EM', text: () => import('./assets-chunks/styles-DUQBEOLS_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 10065, hash: 'ec77d8dc5444ca932680f413763787bf1076f9a904e3cfa6f58233898da9f1fe', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1199, hash: '18cef232ab6e8eee2933c0efe9ec4a1f6b690b4be988e9f1fd24ad7ec9a405c2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 46154, hash: '0b13bb74aec5084695d7c1daaf7aef72a64652be53ad09f57ca6d6509c4ae691', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-K4ZDVGAZ.css': {size: 408933, hash: 'w3/AhGTsNFU', text: () => import('./assets-chunks/styles-K4ZDVGAZ_css.mjs').then(m => m.default)}
   },
 };
--- docs/server/assets-chunks/index_server_html.mjs
+++ docs/server/assets-chunks/index_server_html.mjs
@@ -6,10 +6,10 @@ export default `<!doctype html>
   <base href="/aayushInstitute/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="styles-DUQBEOLS.css"><link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" as="style"></head>
+<link rel="stylesheet" href="styles-K4ZDVGAZ.css"><link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" as="style"></head>
 <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
 </script>
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-KIS6ZRHZ.js" defer=""></script><script src="main-NHLQJUCF.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-KIS6ZRHZ.js" defer=""></script><script src="main-LS6KUCDR.js" type="module"></script></body>
 </html>
 `;
